a2a8e7df808fd16c9132b432c2c6537b21b9fca9
Manish Verma <manish.verma@mintworxs.co>
2025-03-04 09:19:14 +0530

    changes in the code

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const cartController = require("../controllers/cartController");
-const authMiddleware = require("../middleware/authMiddleware"); // Ensure user is logged in
+// const authMiddleware = require("../middleware/authMiddleware"); // Ensure user is logged in
 
-router.post("/add", authMiddleware, cartController.addToCart);
-router.get("/", authMiddleware, cartController.getCart);
-router.delete("/remove", authMiddleware, cartController.removeFromCart);
+// router.post("/add", authMiddleware, cartController.addToCart);
+// router.get("/", authMiddleware, cartController.getCart);
+// router.delete("/remove", authMiddleware, cartController.removeFromCart);
 
 
 
