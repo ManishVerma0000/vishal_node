8cb253e0e9f735b33288e01499e01cad17d1076c
Vishal-Ravan <108457619+Vishal-Ravan@users.noreply.github.com>
2025-03-06 00:52:53 +0530

    some changes

--- server.js
+++ server.js
@@ -2,18 +2,18 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const cors = require("cors");
-// const productRoutes = require("./routes/productRoutes");
-// const susbscribeRoutes = require("./routes/susbscribeRoutes");
-// const registerRoutes = require("./routes/registerRoutes");
-// const contactRoutes = require("./routes/contactRoutes");
-// const authRoutes = require("./routes/authRoutes");
-// const cartRoutes = require("./routes/cartRoutes");
-// const checkoutRoutes = require("./routes/checkoutRoutes");
-// const userRoutes = require("./routes/userRoutes");
-// const orderRoutes = require("./routes/orderRoutes");
-// const adminuserRoutes = require("./routes/adminuserRoutes");
-// const wishlistRoutes = require("./routes/wishlistRoutes");
-// const shippingRoutes = require('./routes/shippingRoutes');
+const productRoutes = require("./routes/productRoutes");
+const susbscribeRoutes = require("./routes/susbscribeRoutes");
+const registerRoutes = require("./routes/registerRoutes");
+const contactRoutes = require("./routes/contactRoutes");
+const authRoutes = require("./routes/authRoutes");
+const cartRoutes = require("./routes/cartRoutes");
+const checkoutRoutes = require("./routes/checkoutRoutes");
+const userRoutes = require("./routes/userRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+const adminuserRoutes = require("./routes/adminuserRoutes");
+const wishlistRoutes = require("./routes/wishlistRoutes");
+const shippingRoutes = require('./routes/shippingRoutes');
 const app = express();
 
 dotenv.config();
@@ -36,24 +36,24 @@ app.use((req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
-  const data = { message: "Welcome to the Express Server!" };
+    const data = { message: "Welcome to the Pittara Server!" };
 
-  console.log("Data sent to client:", data); // Print data in the console
-  res.json(data); // Send JSON response to the client
+    console.log("Data sent to client:", data); // Print data in the console
+    res.json(data); // Send JSON response to the client
 })
-// app.use("/api/product", productRoutes);
-// app.use("/api/subscribe", susbscribeRoutes);
-// app.use("/api/contact", contactRoutes);
-// app.use("/api/login", authRoutes);
-// app.use("/api/register", registerRoutes );
-// app.use("/api/cart", cartRoutes);
-// app.use("/api/checkout", checkoutRoutes);
-// app.use("/api/users", userRoutes);
-// app.use("/api/users", userRoutes);
-// app.use("/api/orders", orderRoutes);
-// app.use("/api/adminuser", adminuserRoutes);
-// app.use("/api/wishlist", wishlistRoutes);
-// app.use('/api/shipping', shippingRoutes);
+app.use("/api/product", productRoutes);
+app.use("/api/subscribe", susbscribeRoutes);
+app.use("/api/contact", contactRoutes);
+app.use("/api/login", authRoutes);
+app.use("/api/register", registerRoutes );
+app.use("/api/cart", cartRoutes);
+app.use("/api/checkout", checkoutRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/adminuser", adminuserRoutes);
+app.use("/api/wishlist", wishlistRoutes);
+app.use('/api/shipping', shippingRoutes);
 app.listen(PORT, () => {
   console.log("running");
 });
